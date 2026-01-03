fe5964e4c68cd51a84c1b9b2ee337dbf9f3fbaea
arifulmist <arifulm926@gmail.com>
2026-01-03 19:04:35 +0600

    userprofile to other profile scrolling issue solve ,layout file have some small change

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom";
-import {TopNav} from "../components/TopNav";
-import {BotNav} from "../components/BotNav";
+import { TopNav } from "../components/TopNav";
+import { BotNav } from "../components/BotNav";
 
 export function Layout() {
   const location = useLocation();
@@ -11,12 +11,18 @@ export function Layout() {
 
   return (
     <>
-      <div className="fixed w-full">
+      <header className="sticky top-0 w-full z-50">
         {!hideNav && <TopNav />}
         {!hideNav && <BotNav />}
-      </div>
+      </header>
 
-      <main className={`${hideNav ? "min-h-screen h-screen flex items-center justify-center" : "flex items-center justify-center"} py-30`}>
+      <main
+        className={`${
+          hideNav
+            ? "min-h-screen h-screen flex items-center justify-center"
+            : "flex items-center justify-center"
+        }`}
+      >
         <Outlet />
       </main>
     </>
